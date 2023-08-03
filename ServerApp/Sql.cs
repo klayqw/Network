d74229587d6b99fa4103715f3321eac351e4d2f0
klayqw <akasiyapika@gmail.com>
2023-08-04 00:59:13 +0400

    Create API

---
+++ ServerApp/Sql.cs
@@ -0,0 +1,57 @@
+﻿using Models;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using System.Formats.Asn1;
+
+internal class Sql
+{
+    private readonly IDbConnection connection;
+
+    public Sql(string connectionString)
+    {
+        this.connection = new SqlConnection(connectionString);
+    }
+
+    public async Task<IEnumerable<User>> FindAllAsync()
+    {
+        var users = await this.connection.QueryAsync<User>($@"select * from Users");
+
+        return users;
+    }
+
+    public async Task<User> FindAsync(int id)
+    {
+        
+            var user = await this.connection.QueryFirstAsync<User>(
+                sql: $@"select * from Users u
+                    where u.Id = @id",
+                param: new { id });
+            return user;
+    }
+
+    public async Task CreateAsync(User user)
+    {
+        await this.connection.ExecuteAsync(
+            sql: @"insert into Users([Login], [Password],[Email])
+                    values(@Login, @Password,@Email)",
+            param: new { user.login, user.password,user.email });
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await this.connection.ExecuteAsync(
+           sql: @"delete from Users
+                  where Users.Id = @id",
+           param: new { id });
+    }
+
+    public async Task UpdateAsync(int id,User user)
+    {
+        await this.connection.ExecuteAsync(
+           sql: @"update Users
+                    Set Login = @login,Password = @password,Email = @email
+                    where Id = @id",
+           param: new { id,user.login,user.password,user.email });
+    }
+}
